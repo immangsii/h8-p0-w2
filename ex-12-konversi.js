04553bc8916ee7f377f29cb3bf2901ce39b2708d
Irman Burhanuddin <irmanburhanuddin@gmail.com>
2019-09-28 12:49:11 +0700

    change name

--- ex-12-konversi.js
+++ ex-12-konversi.js
@@ -10,10 +10,3 @@ function konversiMenit(menit) {
   console.log(konversiMenit(53)); // 0:53
   console.log(konversiMenit(88)); // 1:28
   console.log(konversiMenit(120)); // 2:00
-
-
-//   var h = Math.floor(menit / 60);
-//   var mm = (menit % 60).toString().length === 1 ? '0' + Math.floor(menit % 60) : Math.floor(menit % 60);
-
-//   return h + ':' + mm;
-// }
